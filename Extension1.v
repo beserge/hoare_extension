0322f2f53f794572999411012cd0d25d0726a98a
beserge <bensergentanis@gmail.com>
2020-03-16 10:42:22 -0400

    first attempt

---
+++ Extension1.v
@@ -0,0 +1,18 @@
+Set Warnings "-notation-overridden,-parsing".
+
+
+(*D:\Development\Coq\plf\common was previously bound to PLF.common; it is remapped to common
+[overriding-logical-loadpath,loadpath]
+D:\Development\Coq\plf\common\css was previously bound to PLF.common.css; it is remapped to common.css
+[overriding-logical-loadpath,loadpath]
+D:\Development\Coq\plf\common\media was previously bound to PLF.common.media; it is remapped to common.media
+[overriding-logical-loadpath,loadpath]
+D:\Development\Coq\plf\common\media\font was previously bound to PLF.common.media.font; it is remapped to
+common.media.font [overriding-logical-loadpath,loadpath]
+D:\Development\Coq\plf\common\media\image was previously bound to PLF.common.media.image; it is remapped to
+common.media.image [overriding-logical-loadpath,loadpath]
+D:\Development\Coq\plf was previously bound to PLF; it is remapped to <> [overriding-logical-loadpath,loadpath]*)
+
+Add LoadPath "D:\Development\Coq\plf".
+
+Require Import Imp.
